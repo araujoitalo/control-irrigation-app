667d0874a5d4878bb2de232a9fc0cb7ae6de74ac
araujoitalo <italogustavoaraujo@gmail.com>
2022-02-17 10:29:13 -0300

    Configuração base servidor;

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,6 +1,6 @@
 export class AppConstants {
 
-	public static get baseServidor(): string { return "http://localhost:8080/" }
+	public static get baseServidor(): string { return "https://controle-irrigacao-api.herokuapp.com/" }
 
 	public static get baseLogin(): string { return this.baseServidor + "controlirrigationapi/login" }
 
